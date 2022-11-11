216e37b352f078a35c2d05676300ba87ba1f93c4
JayaNagendra <lankalapallinagendra@gmail.com>
2022-11-11 14:12:49 +0530

    file send

---
+++ Test_array.java
@@ -0,0 +1,13 @@
+public class Test_array{   // basic single dimensional
+public static void main (String[] args){
+int[] arr =new int[5];
+arr[0]=1;
+arr[1]=4;
+arr[2]=6;
+arr[3]=8;
+
+for(int i=0;i<arr.length;i++){
+System.out.println(arr[i]);
+}
+}
+}
